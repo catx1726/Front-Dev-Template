f7dca33b01b0bb4a01438fba5b7469fcee44835d
catx1726 <catx1726@foxmail.com>
2020-05-21 09:37:19 +0800

    2020年5月21日

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <MyHeader />
+    <Nuxt class="content-container" />
+    <MyFooter />
+  </v-app>
+</template>
+
+<script>
+import MyHeader from '~/components/MyHeader'
+import MyFooter from '~/components/MyFooter'
+
+export default {
+  components: {
+    MyHeader,
+    MyFooter
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+.content-container {
+  height: 80vh;
+  font-size: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+  color: white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="index-container">
+    Index
+  </main>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.index-container {
+  background-size: contain;
+  background-position: center;
+  background-image: url('/imgs/TV-80 玲.png');
+}
+</style>
